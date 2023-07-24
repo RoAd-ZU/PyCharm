295a20188bae3deb3b0b925e06dd61345f32c996
RoAd-ZU <ZUZERN.AD@gmail.com>
2023-07-24 04:03:36 +0300

    main-

---
+++ funcs.py
@@ -0,0 +1,5 @@
+def sum_the_numbers(x, y):
+    print(x + y)
+
+def subtract_numbers(x, y):
+    print(x - y)
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-from funcs import sum_the_numbers
+from funcs import sum_the_numbers, subtract_numbers
 
 sum_the_numbers(2, 3)
+
+subtract_numbers(5, 2)
